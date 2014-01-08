c6b10179dc04ceedb7273b9dec382d402034755f
Cyrill Schumacher <cyrill@schumacher.fm>
2014-01-08 17:05:06 +1100

    Adding true === to file_exists

--- app/code/community/Varien/Autoload.php
+++ app/code/community/Varien/Autoload.php
@@ -160,11 +160,11 @@ class Varien_Autoload
             if ($value !== FALSE) {
                 self::setCache($value);
             }
-        } elseif (file_exists(self::getCacheFilePath())) {
+        } elseif (true === file_exists(self::getCacheFilePath())) {
             self::setCache(unserialize(file_get_contents(self::getCacheFilePath())));
         }
 
-        if (file_exists(self::getRevalidateFlagPath()) && unlink(self::getRevalidateFlagPath())) {
+        if (true === file_exists(self::getRevalidateFlagPath()) && unlink(self::getRevalidateFlagPath())) {
             // When this is called there might not be an autoloader in place. So we need to manually load all the needed classes:
             require_once implode(DIRECTORY_SEPARATOR, array(self::$_BP, 'app', 'code', 'core', 'Mage', 'Core', 'Helper', 'Abstract.php'));
             require_once implode(DIRECTORY_SEPARATOR, array(self::$_BP, 'app', 'code', 'local', 'Aoe', 'ClassPathCache', 'Helper', 'Data.php'));
